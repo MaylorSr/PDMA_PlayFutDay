316ba19c3c4e535d0ce1ae230a8f9c4560f78050
Maylor David Bustamante Mercado <maylorbustamante2001@gmail.com>
2023-04-07 17:11:30 +0200

    Login realizado con proteccion de rutas

---
+++ playfutday_web/src/app/access.guard.ts
@@ -0,0 +1,47 @@
+import { Injectable, OnInit } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from './_services/token-storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccessGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private tokenService: TokenStorageService
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.checkUserLoginAdmin(route);
+  }
+
+  checkUserLoginAdmin(route: ActivatedRouteSnapshot): boolean {
+    console.log(this.tokenService.getUser());
+    const { roles = [] } = this.tokenService.getUser();
+
+    if (this.tokenService.getToken() == null) {
+      this.router.navigate(['auth']);
+      return false;
+    } else if (
+      this.tokenService.getToken != null &&
+      !roles.includes(route.data['role'])
+    ) {
+      this.router.navigate(['/', 'acces_denied']);
+      return false;
+    }
+    return true;
+  }
+}
